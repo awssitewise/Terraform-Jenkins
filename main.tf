1cdc4f86eb9677e8e2d4b41370a63671814e10b6
awssitewise <125352821+awssitewise@users.noreply.github.com>
2023-02-14 14:35:12 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-082b1f4237bd816a1" # us-west-2
   instance_type = "t2.micro"  
   tags = {
       Name = "TF-Instance"
